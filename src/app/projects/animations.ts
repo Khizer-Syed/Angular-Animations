1354c00ae8a5bfd957869c19a926519b33c80308
khizerrocks.demon@gmail.com <ksyed@connexservice.ca>
2020-10-27 23:10:54 -0400

    Final Commit

---
+++ src/app/projects/animations.ts
@@ -0,0 +1,101 @@
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations'; 
+
+export const markedTrigger = trigger('markedState', [
+    state('default', style({
+      border: '1px solid black',
+      backgroundColor: 'transparent',
+      padding: '20px'
+    })),
+    state('marked', style({
+      border: '2px solid blue',
+      backgroundColor: '#caeff9',
+      padding: '19px'
+    })),
+    transition('default => marked', [
+        style({
+            border : '2px solid black',
+            padding: '19px'
+        }),
+        animate('200ms ease-out', style({
+            transform: 'scale(1.02)'
+        })),
+        animate(200)]),
+    transition('marked => default', [
+        style({
+            border : '1px solid blue',
+            padding: '20px'
+        }),
+        animate('0.3s ease-out')])
+  ]);
+
+  export const itemStateTrigger = trigger('itemState', [
+      transition(':enter', [
+          style({
+              opacity : 0,
+              transform: 'translateX(-100%)'
+          }),
+          animate('500ms ease-out', keyframes([
+              style({
+                  opacity: 0,
+                  transform: 'translateX(-100%)',
+                  offset: 0
+              }),
+              style({
+                opacity: 1,
+                transform: 'translateX(15%)',
+                offset: 0.4
+              }),
+              style({
+                opacity: 1,
+                transform: 'translateX(0)',
+                offset: 1
+            })
+          ]))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in', keyframes([
+            style({
+          opacity : 1,
+          transform: 'translateX(0)'
+        }),
+        style({
+            opacity : 1,
+            transform: 'translateX(-15%)'
+          }),
+          style({
+            opacity : 0,
+            transform: 'translateX(100%)'
+          })
+        ])
+    )
+  ]),
+  transition('slidUp => slidDown', [
+    style({
+        transform: 'translateY(-100%)'
+    }),
+    animate('300ms ease-out', style({
+        transform: 'translateY(0)'
+    }))
+  ]),
+  transition('slidDown => slidUp', [
+    animate('300ms ease-out', style({
+        transform: 'translateY(-100%)'
+    }))
+  ])
+]);
+
+export const slideStateTrigger = trigger('slideState', [
+    transition(':enter', [
+        style({
+            transform: 'translateY(-100%)'
+        }),
+        animate('300ms ease-out', style({
+            transform: 'translateY(0)'
+        }))
+    ]),
+    transition(':leave', [
+        animate('300ms ease-out', style({
+            transform: 'translateY(-100%)'
+        }))
+    ])
+])
---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { AnimationEvent } from '@angular/animations'; 
+import { Project } from './project.model';
+
+import { ProjectsService } from './projects.service';
+import { markedTrigger, itemStateTrigger, slideStateTrigger } from './animations';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css'],
+  animations : [
+    markedTrigger,
+    itemStateTrigger,
+    slideStateTrigger
+  ]
+})
+export class ProjectsComponent implements OnInit {
+  projects: Project[];
+  displayedProjects: Project[] =[];
+  markedPrjIndex = 0;
+  progress = 'progressing';
+  createNew = false;
+
+  constructor(private prjService: ProjectsService) { }
+
+  ngOnInit() {
+    this.prjService.loadProjects()
+      .subscribe(
+        (prj: Project[]) => {
+          this.progress = 'finished';
+          this.projects = prj;
+          if(this.projects.length >= 1) {
+            this.displayedProjects.push(this.projects[0]);
+          }
+        }
+      );
+  }
+
+  onItemAnimated(animationEvent: AnimationEvent, lastProjectId: number) {
+    if(animationEvent.fromState != 'void') {
+      return;
+    }
+
+    if(this.projects.length > lastProjectId + 1) {
+      this.displayedProjects.push(this.projects[lastProjectId + 1])
+    } else {
+      this.projects = this.displayedProjects;
+    }
+  }
+
+
+  onStatusUpdated(newStatus: string, id: number) {
+    this.projects[id].status = newStatus;
+  }
+
+  onProjectDeleted(index: number) {
+    this.projects.splice(index, 1);
+  }
+
+  onProjectCreated(project: Project) {
+    this.createNew = false;
+    setTimeout(() => {
+      this.projects.unshift(project);
+    },300);
+   
+  }
+}
